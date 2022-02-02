b944e5b3b843e3f3e70608245f2d1305cb035ded
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-02-02 09:28:08 -0300

    Primeiro exercicio de variaveis em JS

---
+++ fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia01-javascript-primeiros-passos/exercicio-01/scripts.js
@@ -0,0 +1,10 @@
+const myName = "Thiago";
+const birthCity = 'Belo Horizonte';
+let birthYear = 1983;
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+birthYear = 2030;
+console.log(birthYear);
+birthCity = "Betim";
+console.log(birthCity); //Não pode alterar o valor de uma constante.
