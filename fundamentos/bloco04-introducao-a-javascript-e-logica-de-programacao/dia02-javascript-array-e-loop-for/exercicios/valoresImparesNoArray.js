b24ae4f812cdf2364d155291c9fcc084997e7121
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-02-03 18:53:31 -0300

    retirei uma variavel desnecessaria

--- fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia02-javascript-array-e-loop-for/exercicios/valoresImparesNoArray.js
+++ fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia02-javascript-array-e-loop-for/exercicios/valoresImparesNoArray.js
@@ -1,6 +1,6 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let resultado = 0;
-let mensagem = 'Nenhum valor encontrado';
+
 for(let number of numbers) {
     if(number % 2 === 1) {
         resultado += 1
