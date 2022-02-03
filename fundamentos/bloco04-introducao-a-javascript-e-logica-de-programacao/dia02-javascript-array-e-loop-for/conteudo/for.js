ad6c81752970b6b0235c3014be1cd7172b8a197f
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-02-03 09:40:41 -0300

    Conteudo laço for

---
+++ fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia02-javascript-array-e-loop-for/conteudo/for.js
@@ -0,0 +1,13 @@
+let cars = ['Saab', 'Volvo', 'BMW'];
+
+for(let index = 0; index < cars.length; index += 1) {
+    console.log(cars[index])
+}
+console.log('')
+//EXERCICIO 1
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for(let i = 0; i < groceryList.length; i += 1) {
+    console.log(groceryList[i])
+}
