fded99743301435b75ec8fdb59dfd3f8554f84d0
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-02-03 11:52:55 -0300

    media aritmetica dos elementos do array

---
+++ fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia02-javascript-array-e-loop-for/conteudo/for-of.js
@@ -0,0 +1,28 @@
+// EXERCICIO 1
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for(let name of names) {
+    console.log(name)
+}
+console.log('')
+
+// CONTEUDO
+
+let numeros = [1,2,3,4,5];
+for(let numero of numeros) {
+  console.log(numero);
+}
+console.log('')
+
+let word = 'Hello';
+for (let letter of word) {
+  console.log(letter);
+}
+console.log('')
+
+let arrOfNumbers = [10, 20, 30];
+for (let sum of arrOfNumbers) {
+  sum += 1;
+  console.log(sum);
+}
+console.log(arrOfNumbers)
