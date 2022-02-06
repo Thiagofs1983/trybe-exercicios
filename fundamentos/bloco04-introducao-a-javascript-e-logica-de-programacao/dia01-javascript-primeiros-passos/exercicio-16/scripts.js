0c4a9f2c55faad948c297d6c875e0c3b8ad6e29a
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-02-06 10:40:33 -0300

    refatorando o codigo de imposto de renda e acrescentando o desconto da Trybe.

--- fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia01-javascript-primeiros-passos/exercicio-16/scripts.js
+++ fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia01-javascript-primeiros-passos/exercicio-16/scripts.js
@@ -1,32 +1,38 @@
+//CALCULO INSS
 const salarioBruto = 3000
 let aliquotaInss = 0;
-let ir = 0;
-
 if (salarioBruto <= 1556.94) {
-    aliquotaInss = salarioBruto * 0.08
+    aliquotaInss = 0.08
 }else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
-    aliquotaInss = salarioBruto * 0.09
+    aliquotaInss = 0.09
 }else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
-    aliquotaInss = salarioBruto * 0.11
+    aliquotaInss = 0.11
 }else {
     aliquotaInss = 570.88
 }
-//console.log(aliquotaInss)
+let salarioBase = salarioBruto > 5189.82 ? salarioBruto - aliquotaInss : salarioBruto - (salarioBruto * aliquotaInss)
 
-let salarioBase = salarioBruto - aliquotaInss
-//console.log(salarioBase)
-
-if (salarioBase < 1903.98) {
-    ir = 0
-}else if (salarioBase > 1903.98 && salarioBase <= 2826.65) {
-    ir = ((salarioBase * 0.075) - 142.8).toFixed(2)
+//CALCULO IR
+let aliquotaIR = 0
+let deducaoIR = 0
+if (salarioBase > 1903.98 && salarioBase <= 2826.65) {
+    aliquotaIR = 0.075
+    deducaoIR = 142.8
 }else if (salarioBase > 2826.65 && salarioBase <= 3751.05) {
-    ir = ((salarioBase * 0.15) - 354.8).toFixed(2)
+    aliquotaIR = 0.15
+    deducaoIR = 354.8
 }else if (salarioBase > 3751.05 && salarioBase <= 4664.68) {
-    ir = ((salarioBase * 0.225) - 636.13).toFixed(2)
+    aliquotaIR = 0.225
+    deducaoIR = 636.13
 }else if (salarioBase > 4664.68) {
-    ir = ((salarioBase * 0.275) - 869.36).toFixed(2)
+    aliquotaIR = 0.275
+    deducaoIR = 869.36
 }
-//console.log(ir)
-const salarioLiquido = salarioBase - ir
-console.log(salarioLiquido)
+
+const descontoTrybe = salarioBruto * 0.17
+const impostoRenda = (salarioBase * aliquotaIR) - deducaoIR
+const salarioLiquido = salarioBase - impostoRenda
+console.log(`Salario liquido é: R$ ${salarioLiquido}`)
+console.log(`Desconto da Trybe é: R$ ${descontoTrybe.toFixed(2)}`)
+const salarioFinal = salarioLiquido - descontoTrybe
+console.log(`Salario Final é: R$ ${salarioFinal}`)
