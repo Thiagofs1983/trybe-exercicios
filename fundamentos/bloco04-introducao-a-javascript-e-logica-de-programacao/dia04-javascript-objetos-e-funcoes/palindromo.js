70481806db71b1232201d724629f8dbdb72aee45
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-02-07 13:28:49 -0300

    retornar o indice do maior valor dentro do array

--- fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia04-javascript-objetos-e-funcoes/palindromo.js
+++
@@ -1,9 +0,0 @@
-function verificaPalindromo(palavra) {
-    if(palavra.split('').reverse().join('') === palavra) {
-        return true
-    }else {
-        return false
-    }
-}
-console.log(verificaPalindromo('arara'))
-console.log(verificaPalindromo('desenvolvimento'))
