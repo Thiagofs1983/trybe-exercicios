053523a0791315b311812eb0068e7718cb07e336
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-02-07 10:47:50 -0300

    juntar os objetos de acordo com cada chave

--- fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia04-javascript-objetos-e-funcoes/exercicios/boasVindas.js
+++ fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia04-javascript-objetos-e-funcoes/exercicios/boasVindas.js
@@ -15,3 +15,26 @@ for(let index in info) {
 for (let index in info) {
     console.log(info[index])
 }
+
+let segundaInfo = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'sim'
+  };
+  /* console.log(info.personagem + ' e ' + segundaInfo.personagem)
+  console.log(`${info.origem} e ${segundaInfo.origem}`)
+  console.log(`${info.nota} e ${segundaInfo.nota}`)
+  info.recorrente === 'sim' && segundaInfo.recorrente === 'sim' ? console.log('Ambos recorrentes') : console.log('Um ou mais não é recorrente') */
+
+  /* for (let itens in info) {
+      if(itens === 'recorrente' && info[itens] === 'sim' && segundaInfo[itens] === 'sim') {
+          console.log('Ambos recorrentes')
+      }else {
+          console.log(info[itens], ' e ', segundaInfo[itens])
+      }
+  } */
+for (let itens in info) {
+    console.log(info[itens], ' e ', segundaInfo[itens])
+}
+  
