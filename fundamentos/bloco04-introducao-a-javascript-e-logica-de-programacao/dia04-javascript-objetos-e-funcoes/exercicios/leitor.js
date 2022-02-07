02745575850b674cf6bf3b51fde8059be9268f1b
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-02-07 12:06:17 -0300

    acessar as chaves nome, sobrenome e titulo dentro do objeto

---
+++ fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia04-javascript-objetos-e-funcoes/exercicios/leitor.js
@@ -0,0 +1,14 @@
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+  let fullName = leitor.nome + ' ' + leitor.sobrenome
+  console.log(`O livro favorito de ${fullName} se chama "${leitor.livrosFavoritos[0].titulo}"`)
