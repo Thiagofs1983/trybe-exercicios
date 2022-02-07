071ed827227aaebf03c8d76fcf731c74a2f39f01
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-02-06 21:07:37 -0300

    adicionar uma chave ao objeto

---
+++ fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia04-javascript-objetos-e-funcoes/conteudo/player.js
@@ -0,0 +1,18 @@
+/* let name = 'Marta';
+let lastName = 'Silva';
+let age = 34;
+let medals = { golden: 2, silver: 3 }; */
+
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: {
+        golden: 2,
+        silver: 3,
+    },
+}
+let nomeCompleto = player.name + ' ' + player.lastName
+console.log(`A jogadora ${nomeCompleto} tem ${player.age} de idade.`)
+
+player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018]
