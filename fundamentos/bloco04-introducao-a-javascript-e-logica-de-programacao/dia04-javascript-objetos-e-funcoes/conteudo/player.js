1f7a878becddc77c87664a297edd9cea98edb0bf
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-02-06 21:13:10 -0300

    acessar a chave adicionada mostrando a quantidade de vezes que Marta foi eleita a melhor jogadora do mundo

--- fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia04-javascript-objetos-e-funcoes/conteudo/player.js
+++ fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia04-javascript-objetos-e-funcoes/conteudo/player.js
@@ -16,3 +16,6 @@ let nomeCompleto = player.name + ' ' + player.lastName
 console.log(`A jogadora ${nomeCompleto} tem ${player.age} de idade.`)
 
 player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018]
+//"A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes".
+let vezesMelhorDoMundo = player.bestInTheWorld.length
+console.log(`A jogadora ${nomeCompleto} foi eleita a melhor jogadora do mundo por ${vezesMelhorDoMundo} vezes.`)
