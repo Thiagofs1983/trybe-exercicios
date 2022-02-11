cb14993e9a8091d96a17c18a00c5086c7460311f
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-02-11 17:49:26 -0300

    colocar a solucao do exercicio 1 dentro da funcao criarDias

--- fundamentos/bloco05-javascript-dom-eventos-e-web-storage/dia03-javascript-eventos/exercicios/script.js
+++ fundamentos/bloco05-javascript-dom-eventos-e-web-storage/dia03-javascript-eventos/exercicios/script.js
@@ -1,35 +1,59 @@
 function createDaysOfTheWeek() {
-    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
-    const weekDaysList = document.querySelector('.week-days');
-  
-    for (let index = 0; index < weekDays.length; index += 1) {
-      const days = weekDays[index];
-      const dayListItem = document.createElement('li');
-      dayListItem.innerHTML = days;
-  
-      weekDaysList.appendChild(dayListItem);
-    };
-  };
-  
-  createDaysOfTheWeek();
-  
-  // Escreva seu código abaixo.
-  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const weekDays = [
+    "Domingo",
+    "Segunda",
+    "Terça",
+    "Quarta",
+    "Quinta",
+    "Sexta",
+    "Sábado",
+  ];
+  const weekDaysList = document.querySelector(".week-days");
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement("li");
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  }
+}
+
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+function criarDias() {
+  const dezDaysList = [
+    29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+    20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+  ];
   for (let index = 0; index < dezDaysList.length; index += 1) {
-    let diasMes = document.getElementById('days')
-    let listaDias = dezDaysList[index]
-    listaDias = document.createElement('li')
-    listaDias.innerHTML = dezDaysList[index]
-    listaDias.className = 'day'
-    diasMes.appendChild(listaDias)
-    if (dezDaysList[index] === 24 || dezDaysList[index] === 25 || dezDaysList[index] === 31) {
-        listaDias.className = 'day holiday'
+    let diasMes = document.getElementById("days");
+    let listaDias = dezDaysList[index];
+    listaDias = document.createElement("li");
+    listaDias.innerHTML = dezDaysList[index];
+    listaDias.className = "day";
+    diasMes.appendChild(listaDias);
+    if (
+      dezDaysList[index] === 24 ||
+      dezDaysList[index] === 25 ||
+      dezDaysList[index] === 31
+    ) {
+      listaDias.className = "day holiday";
+    }
+    if (
+      dezDaysList[index] === 4 ||
+      dezDaysList[index] === 11 ||
+      dezDaysList[index] === 18 ||
+      dezDaysList[index] === 25
+    ) {
+      listaDias.className = "day friday";
+    }
+    if (dezDaysList[index] === 25) {
+      listaDias.className = "day holiday friday";
     }
   }
+}
+criarDias()
 
-  for (let index = 0; index < dezDaysList.length; index += 1) {
-      
-
-  }
-
-  console.log(document.querySelector('body'))
+console.log(document.querySelector("body"));
