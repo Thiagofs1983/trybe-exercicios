9e4db5f57650d6099f81a6fa6518982ab1d1c986
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-02-18 09:56:00 -0300

    adicionar um preventdefault no input sendo que so possa digitar a letra a minuscula

--- fundamentos/bloco06-html-e-css-forms-flexbox-e-responsivo/dia01-html-e-css-forms/conteudo2/script.js
+++ fundamentos/bloco06-html-e-css-forms-flexbox-e-responsivo/dia01-html-e-css-forms/conteudo2/script.js
@@ -3,6 +3,15 @@ const INPUT_TEXT = document.querySelector('#input-text');
 const INPUT_CHECKBOX = document.querySelector('#input-checkbox');
 const HREF_LINK = document.querySelector('#href');
 
-HREF_LINK.addEventListener('click', function(event){
+function preventDefault(event){
     event.preventDefault()
+}
+
+HREF_LINK.addEventListener('click', preventDefault)
+INPUT_CHECKBOX.addEventListener('click', preventDefault)
+INPUT_TEXT.addEventListener('keypress', function(event){
+    let caracter = event.key;
+    if(caracter !== 'a'){
+        event.preventDefault()
+    }
 })
