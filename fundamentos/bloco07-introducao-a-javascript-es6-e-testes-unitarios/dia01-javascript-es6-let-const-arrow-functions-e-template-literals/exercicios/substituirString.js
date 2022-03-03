bd382a1accbafce136724325c413c9f24e522eea
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-03-03 20:05:42 -0300

    concatenar o resultado da primeira funcao com o array de skills

--- fundamentos/bloco07-introducao-a-javascript-es6-e-testes-unitarios/dia01-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios/substituirString.js
+++ fundamentos/bloco07-introducao-a-javascript-es6-e-testes-unitarios/dia01-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios/substituirString.js
@@ -1,8 +1,5 @@
 const skills = ['JavaScript', 'CSS', 'HTML', 'DOM', 'FlexBox'];
 
-const recebeString = (str, strDeterminada) => strDeterminada.replace(/x/i, str)
-
-const concatenaInfo = () => `${recebeString('Thiago', 'Tryber x aqui!')} Minhas cinco principais habilidades são:
-${skills.sort()}.`
-
-console.log(concatenaInfo())
+const recebeString = (str) => `Tryber ${str} aqui! Minhas cinco principais habilidades são:
+${skills.sort()}. #goTrybe`
+console.log(recebeString('Thiago')) 
