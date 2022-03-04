780c52a0f11004a6ecf9ba9ba551ec7b733ae5d0
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-03-04 16:04:42 -0300

    refatorando a ultima questao

--- fundamentos/bloco07-introducao-a-javascript-es6-e-testes-unitarios/dia01-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios/substituirString.js
+++ fundamentos/bloco07-introducao-a-javascript-es6-e-testes-unitarios/dia01-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios/substituirString.js
@@ -1,5 +1,10 @@
 const skills = ['JavaScript', 'CSS', 'HTML', 'DOM', 'FlexBox'];
 
-const recebeString = (str) => `Tryber ${str} aqui! Minhas cinco principais habilidades são:
-${skills.sort()}. #goTrybe`
-console.log(recebeString('Thiago')) 
+const recebeString = (str) => `Tryber ${str} aqui!` //Minhas cinco principais habilidades são:${skills.sort()}. #goTrybe`
+
+const frasecompleta = (recebeString) => {
+
+    return `${recebeString} Minhas cinco principais habilidades são:${skills.sort()}. #goTrybe`
+}
+console.log(frasecompleta(recebeString('Thiago'))) 
+
