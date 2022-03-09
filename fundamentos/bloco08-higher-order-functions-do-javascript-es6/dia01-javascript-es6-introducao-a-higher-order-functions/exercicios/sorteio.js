130a4d2316cdecc327c5d5948daaa9233b390ff7
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-03-09 15:58:37 -0300

    HOF que retorna o resultado de um sorteio

---
+++ fundamentos/bloco08-higher-order-functions-do-javascript-es6/dia01-javascript-es6-introducao-a-higher-order-functions/exercicios/sorteio.js
@@ -0,0 +1,15 @@
+/* 
+1 - HOF que retorna o resultado de um sorteio - ok
+2 - HOF gera um numero aleatório entre 1 e 5 - ok
+3 - HOF recebe como paramentos o numero apostado e uma função - ok
+4 - A função checa se o número apostado é igual ao número sorteado - ok
+5 - Retorno da HOF deve ser uma string('Tente novamente' ou 'Parabens você ganhou') - ok */
+
+const resultSorteio = (numAposta, numSorteado) => numAposta === numSorteado ? 'Parabéns, você ganhou!' : 'Tente novamente'
+
+const sorteio = (numAposta, func) => {
+    let numAleatorio = Math.floor(Math.random() * 5 + 1)
+    console.log(numAleatorio)
+    return func(numAposta, numAleatorio)
+}
+console.log(sorteio(3, resultSorteio))
