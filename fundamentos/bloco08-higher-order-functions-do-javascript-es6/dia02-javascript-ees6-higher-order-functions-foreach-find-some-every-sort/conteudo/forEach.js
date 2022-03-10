f4ac08398e45b2bf45e2a855eac3102cbf10cc36
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-03-10 11:58:42 -0300

    conteudo de forEach

--- fundamentos/bloco08-higher-order-functions-do-javascript-es6/dia02-javascript-ees6-higher-order-functions-foreach-find-some-every-sort/conteudo/forEach.js
+++ fundamentos/bloco08-higher-order-functions-do-javascript-es6/dia02-javascript-ees6-higher-order-functions-foreach-find-some-every-sort/conteudo/forEach.js
@@ -26,7 +26,7 @@
   //   { name: 'Ana', grade: 81, approved: 'Aprovado' }
   // ] */
 
-  const students = [
+/*   const students = [
     { name: 'Maria', grade: 70, approved: '' },
     { name: 'José', grade: 56, approved: '' },
     { name: 'Roberto', grade: 90, approved: '' },
@@ -51,4 +51,62 @@
   //   { name: 'José', grade: 56, approved: 'Recuperação' },
   //   { name: 'Roberto', grade: 90, approved: 'Aprovado' },
   //   { name: 'Ana', grade: 81, approved: 'Aprovado' }
-  // ]
+  // ] */
+
+/* const arrayOfValues = ['josé', 50, 0.25, { comida: 'Chocolate' }];
+arrayOfValues.forEach((element) => console.log('Cada elemento do array:', element)); */
+
+// Cada elemento do array: josé
+// Cada elemento do array: 50
+// Cada elemento do array: 0.25
+// Cada elemento do array: { comida: 'Chocolate' }
+
+/* const numbers = [11, 24, 39, 47, 50, 62, 75, 81, 96, 100];
+numbers.forEach((element) => element % 2 === 0 ? console.log(`${element} é par`) : 
+console.log(`${element} é ímpar`)) */
+
+/* const arrayOfValues = ['josé', 50, 0.25, { comida: 'Chocolate' }];
+arrayOfValues.forEach((element, indexOfTheArray, theEntireArray) => {
+  console.log('Cada elemento do array:', element);
+  console.log('Index, posição do elemento:', indexOfTheArray);
+  console.log('Array percorrido:', theEntireArray);
+}); */
+
+/* const numbers = [11, 24, 39, 47, 50, 62, 75, 81, 96, 100];
+
+const numerosPares = numeros => {
+    if (numeros % 2 === 0){
+        console.log(`${numeros} é par`)
+    }
+}
+const numerosimpares = numeros => {
+    if (numeros % 2 !== 0) {
+        console.log(`${numeros} é ímpar`)
+    }
+}
+
+numbers.forEach(numerosPares)
+numbers.forEach(numerosimpares)
+
+
+const names = ['Thiago', 'Barbara', 'Heitor']
+
+const transfMaiusculo = (name, index) => names[index] = name.toUpperCase()
+
+const transfMinusculo = (name, index) => names[index] = name.toLowerCase()
+
+names.forEach(transfMinusculo)
+console.log(names) */
+
+const emailListInData = [
+    'roberta@email.com',
+    'paulo@email.com',
+    'anaroberta@email.com',
+    'fabiano@email.com',
+  ];
+  
+  const showEmailList = (email) => {
+    console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+  };
+  
+emailListInData.forEach(showEmailList)
