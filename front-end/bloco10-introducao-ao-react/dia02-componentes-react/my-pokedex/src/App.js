aa88388621f50b9415a9beaf7a65d15ee3756473
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-04-06 09:22:28 -0300

    exercicios do dia 10.2

---
+++ front-end/bloco10-introducao-ao-react/dia02-componentes-react/my-pokedex/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header";
+import Pokedex from "./components/Pokedex";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <Header />
+      <Pokedex />
+    </main>
+  );
+}
+
+export default App;
---
+++ front-end/bloco10-introducao-ao-react/dia02-componentes-react/my-pokedex/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Header.css'
+
+class Header extends Component {
+    render() {
+        return <header className='header-container'><h1 className='title'>Pokedex</h1></header>
+    }
+}
+
+export default Header
