e27339181c4c6bd42a794042adaaa989e43951ca
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-07-27 11:14:24 -0300

    feat: categoria que a pessoa se enquadra através do imc

--- back-end/bloco22-introducao-ao-desenvolvimento-web-com-node/dia01-nodejs-um-motor-javascript/exercicios/imc.js
+++ back-end/bloco22-introducao-ao-desenvolvimento-web-com-node/dia01-nodejs-um-motor-javascript/exercicios/imc.js
@@ -10,7 +10,22 @@ const main = () => {
   const height = readline.questionInt('Qual a sua altura? (cm) ');
 
   const imc = calcImc(weight, height);
+
   console.log(`IMC: ${imc.toFixed(2)}`);
+
+  if(imc < 18.5) {
+    console.log('Abaixo do peso (magreza');
+  } if (imc >= 18.5 && imc < 24.9) {
+    console.log('Peso normal');
+  } if (imc >= 24.9 && imc < 29.9) {
+    console.log('Acima do peso (sobrepeso)');
+  } if (imc >= 29.9 && imc < 34.9) {
+    console.log('Obesidade grau I');
+  } if (imc >= 34.9 && imc < 39.9) {
+    console.log('Obesidade grau II');
+  } if (imc >= 39.9) {
+    console.log('Obesidade graus III e IV');
+  }
 }
 
 main();
